aa21ba921840b4075c8b9582dfc83a02fd6eb765
shloop <shloop@shloop.net>
2026-04-26 18:40:13 -0700

    test chocolatey package creation

---
+++ pkg/choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿$toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
+$z32="$toolsDir/google-book-scraper-$VERSION-windows-x86-portable.zip"
+$z64="$toolsDir/google-book-scraper-$VERSION-windows-x64-portable.zip"
+Get-ChocolateyUnzip -FileFullPath $z32 -FileFullPath64 $z64 -Destination $toolsDir
+Remove-Item $z32
+Remove-Item $z64
