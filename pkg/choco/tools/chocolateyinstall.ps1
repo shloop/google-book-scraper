9112a280bc6860cb3cdde90f1c690529997e0e87
shloop <shloop@shloop.net>
2026-04-29 16:28:25 -0700

    Fix chocolatey publishing (#26)

--- pkg/choco/tools/chocolateyinstall.ps1
+++ pkg/choco/tools/chocolateyinstall.ps1
@@ -4,3 +4,5 @@ $z64="$toolsDir/google-book-scraper-$VERSION-windows-x64-portable.zip"
 Get-ChocolateyUnzip -FileFullPath $z32 -FileFullPath64 $z64 -Destination $toolsDir
 Remove-Item $z32
 Remove-Item $z64
+Remove-Item $toolsDir/LICENSE-APACHE
+Remove-Item $toolsDir/LICENSE-MIT
